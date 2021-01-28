d8938ed33053f420aac535058e8efb5439bde6f3
Daniel Lee <leedanieluk@gmail.com>
2021-01-28 22:09:57 +0000

    Remove unnecessary files

--- src/main/java/VNN/ActivationFunctions.java
+++ src/main/java/VNN/ActivationFunctions.java
@@ -2,17 +2,19 @@ package VNN;
 
 public class ActivationFunctions {
     public static Vector sigmoid(Vector vector) {
+        float[] resultValues = new float[vector.getValues().length];
         for(int element = 0; element < vector.getValues().length; element++) {
-            vector.getValues()[element] = computeSigmoid(vector.getValues()[element]);
+            resultValues[element] = computeSigmoid(vector.getValues()[element]);
         }
-        return vector;
+        return new Vector(resultValues);
     }
 
     public static Vector sigmoidDerivative(Vector vector) {
+        float[] resultValues = new float[vector.getValues().length];
         for(int element = 0; element < vector.getValues().length; element++) {
-            vector.getValues()[element] = computeSigmoid(vector.getValues()[element]) * (1 - computeSigmoid(vector.getValues()[element]));
+            resultValues[element] = computeSigmoid(vector.getValues()[element]) * (1 - computeSigmoid(vector.getValues()[element]));
         }
-        return vector;
+        return new Vector(resultValues);
     }
 
     private static float computeSigmoid(float x) {
